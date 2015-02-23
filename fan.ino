eb07d7afe3ef9689f3423d512bdcec2a3e373272
Jan Wojciechowski <blady992@gmail.com>
2015-02-23 19:43:20 +0100

    added cache for fan speed to avoid interrupting pwm

--- fan.ino
+++ fan.ino
@@ -1,9 +1,13 @@
 #define FAN_PIN 3
 
+byte fan_speed_cache = 0;
+
 void init_fan() {
   pinMode(FAN_PIN, OUTPUT);
 }
 
 void update_fan_speed() {
-  analogWrite(FAN_PIN, spin);
+  if (spin != fan_speed_cache) {
+    analogWrite(FAN_PIN, spin);
+  }
 }
